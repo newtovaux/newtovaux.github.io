a39624dc92c5bb1fe16e6db0dd03452811637a86
newtovaux <binaryfrost@gmail.com>
2021-04-02 20:05:37 +0100

    WIP

--- docs/_nuxt/static/1617389587/composition-api-in-vue/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617389587",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomposition-api-in-vue",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"780d8857"}}}}(null));
